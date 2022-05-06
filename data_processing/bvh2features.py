f1b92f14ea0752a6b2e7ebf8cb138ae73607aef7
Svito-Zar <taras.svitozar@gmail.com>
2022-05-06 12:31:42 +0200

    Update BVH features encoding script

--- data_processing/bvh2features.py
+++ data_processing/bvh2features.py
@@ -21,6 +21,7 @@ from pymo.viz_tools import *
 from pymo.writers import *
 
 import joblib as jl
+import glob
 
 
 def extract_joint_angles(bvh_dir, files, dest_dir, pipeline_dir, fps):
@@ -28,7 +29,7 @@ def extract_joint_angles(bvh_dir, files, dest_dir, pipeline_dir, fps):
 
     data_all = list()
     for f in files:
-        ff = os.path.join(bvh_dir, f + '.bvh')
+        ff = os.path.join(bvh_dir, f)
         print(ff)
         data_all.append(p.parse(ff))
 
@@ -36,9 +37,7 @@ def extract_joint_angles(bvh_dir, files, dest_dir, pipeline_dir, fps):
        ('dwnsampl', DownSampler(tgt_fps=fps,  keep_all=False)),
        ('root', RootTransformer('hip_centric')),
        ('mir', Mirror(axis='X', append=True)),
-       ('jtsel', JointSelector(['Spine','Spine1','Spine2','Spine3','Neck','Neck1','Head','RightShoulder', 'RightArm', 'RightForeArm', 'RightHand', 'LeftShoulder', 'LeftArm', 'LeftForeArm', 'LeftHand'], include_root=True)),
-       ('exp', MocapParameterizer('expmap')), 
-       ('cnst', ConstantsRemover()),
+       ('exp', MocapParameterizer('expmap')),
        ('np', Numpyfier())
     ])
 
@@ -75,12 +74,6 @@ if __name__ == '__main__':
     files = []
     # Go over all BVH files
     print("Going to pre-process the following motion files:")
-    for r, d, f in os.walk(params.bvh_dir):
-        for file in f:
-            print(file)
-            if '.bvh' in file:
-                ff=os.path.join(r, file)
-                basename = os.path.splitext(os.path.basename(ff))[0]
-                files.append(basename)
+    files = sorted([f for f in glob.iglob(params.bvh_dir+'/*.bvh')])
 
-    extract_joint_angles(params.bvh_dir, files, params.dest_dir, params.pipeline_dir , fps=20)
+    extract_joint_angles(params.bvh_dir, files, params.dest_dir, params.pipeline_dir , fps=30)
